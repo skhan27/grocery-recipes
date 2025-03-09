fc6a9cdf08c853981e672b389f1098b9b7210b6c
saimkhan <khans54.2028@gmail.com>
2025-03-09 11:29:42 -0400

    use build js file to populate the environment file

--- grocery-app/src/environments/environment.ts
+++ grocery-app/src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: process.env['FIREBASE_API_KEY'],
-    authDomain: process.env['FIREBASE_AUTH_DOMAIN'],
-    projectId: process.env['FIREBASE_PROJECT_ID'],
-    storageBucket: process.env['FIREBASE_STORAGE_BUCKET'],
-    messagingSenderId: process.env['FIREBASE_MESSAGING_SENDER_ID'],
-    appId: process.env['FIREBASE_APP_ID'],
-    measurementId: process.env['FIREBASE_MEASUREMENT_ID'],
+    apiKey: 'PLACEHOLDER_API_KEY',
+    authDomain: 'PLACEHOLDER_AUTH_DOMAIN',
+    projectId: 'PLACEHOLDER_PROJECT_ID',
+    storageBucket: 'PLACEHOLDER_STORAGE_BUCKET',
+    messagingSenderId: 'PLACEHOLDER_MESSAGING_SENDER_ID',
+    appId: 'PLACEHOLDER_APP_ID',
+    measurementId: 'PLACEHOLDER_MEASUREMENT_ID',
   },
 };
