2bed8c5d6c0d9478fa49ab780f6ce2d1e460c667
Saim Khan <saimkhan@saims-air.home>
2025-04-16 06:09:04 -0400

    add rudimentary table for recipe list

--- grocery-app/src/app/components/recipe-list/recipe-list.component.ts
+++ grocery-app/src/app/components/recipe-list/recipe-list.component.ts
@@ -3,6 +3,7 @@ import { RecipeService } from '../../services/recipe.service';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { Router } from '@angular/router';
 import { RecipeFirebaseService } from '../../services/recipe.firebase.service';
+import { Recipe } from '../../models/recipe';
 
 @Component({
   selector: 'app-recipe-list',
@@ -16,8 +17,26 @@ export class RecipeListComponent implements OnInit {
     initialValue: [],
   });
 
+
+  allColumns: (keyof Recipe)[] = ['name', 'tags', 'rating', 'notes', 'instructions', 'servings', 'prepTime', 'cookTime'];
+  enabledColumns: string[] = ['name', 'tags', 'rating']; // Default enabled columns
+  menuExpanded: boolean = false;
   router = inject(Router);
+  
   ngOnInit(): void {}
+  
+  toggleMenu() {
+    this.menuExpanded = !this.menuExpanded;
+  }
+
+  toggleColumn(column: string) {
+    const index = this.enabledColumns.indexOf(column);
+    if (index === -1) {
+      this.enabledColumns.push(column);
+    } else {
+      this.enabledColumns.splice(index, 1);
+    }
+  }
 
   viewRecipeDetails(recipeId: string): void {
     this.router.navigate(['/recipe-details', recipeId]);
